24ca7fe6d212062818c2a15049dc8020c0ea95f2
Rico Dittrich <ricod1996@gmail.com>
2015-05-24 00:05:32 +0200

    Restructure Skin class, extend XML schema

---
+++ SteamSkinInstaller/Skin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamSkinInstaller {
+    class Skin {
+        public string Author;
+        public string Name;
+        public string Description;
+        public string Website;
+
+        public class DownloadInfo {
+            public string Method;
+            public string DeviantURL;
+            public string GithubUser;
+            public string GithubRepo;
+            public bool GithubUseTags;
+        }
+
+        public DownloadInfo Download;
+
+        public class VersionInfo {
+            public string MatchURL;
+            public string MatchPattern;
+            public int MatchGroup;
+        }
+
+        public VersionInfo Version;
+
+        public class ExtraInfo {
+            public class Font {
+                public string Filename;
+                public string Fontname;
+            }
+
+            public List<Font> Fonts;
+        }
+    }
+}
