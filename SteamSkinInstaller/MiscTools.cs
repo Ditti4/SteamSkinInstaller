a493c4145eb1e30df27adb65d38143e1a68da325
Rico Dittrich <ricod1996@gmail.com>
2015-08-18 20:46:53 +0200

    Cleanup imports, update project file
    Long time, no see. This shall change now! Just pushing out some of the
    work I had locally only for the last few months so we can finally get this
    baby ready.

--- SteamSkinInstaller/MiscTools.cs
+++ SteamSkinInstaller/MiscTools.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-using System.IO.Compression;
 using System.Net.NetworkInformation;
 using System.Windows;
 using System.Windows.Media.Imaging;
--- SteamSkinInstaller/Skin.cs
+++ SteamSkinInstaller/Skin.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
+using SteamSkinInstaller.DownloadHandler;
 
 namespace SteamSkinInstaller {
     class Skin {
