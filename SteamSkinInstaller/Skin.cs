775fca033357918f73e64057b4be9a5d1ff2cbda
Rico Dittrich <ricod1996@gmail.com>
2015-08-18 21:40:34 +0200

    Refactor Skin class, add dummy methods
    Yay, dummy methods. Someone (not me (probalby me anyway)) needs to fill
    them and probably add more. We'll see about that.

--- SteamSkinInstaller/Skin.cs
+++ SteamSkinInstaller/Skin.cs
@@ -5,29 +5,23 @@ using System.Linq.Expressions;
 using SteamSkinInstaller.DownloadHandler;
 
 namespace SteamSkinInstaller {
-    class Skin {
-        public string Author;
-        public string Name;
-        public string Description;
-        public string Website;
+    public class Skin {
+        private IDownload _downloadHandler;
 
         public class DownloadInfo {
             public string Method;
             public string DeviantURL;
+            public string DirectURL;
             public string GithubUser;
             public string GithubRepo;
             public bool GithubUseTags;
         }
-
-        public DownloadInfo Download;
 
         public class VersionInfo {
             public string MatchURL;
             public string MatchPattern;
             public int MatchGroup;
         }
-
-        public VersionInfo Version;
 
         public class ExtraInfo {
             public class Font {
@@ -36,6 +30,84 @@ namespace SteamSkinInstaller {
             }
 
             public List<Font> Fonts;
+        }
+
+        public string Author;
+        public string Name;
+        public string Description;
+        public string Website;
+
+        public DownloadInfo FileDownload;
+        public VersionInfo RemoteVersionInfo;
+        public VersionInfo LocalVersionInfo;
+        public ExtraInfo ExtraStuff;
+
+        private void CreateDownloadHandler() {
+            if (_downloadHandler == null) {
+                switch (FileDownload.Method.ToLower()) {
+                    case "github":
+                        _downloadHandler = new GitHubDownload(FileDownload.GithubUser, FileDownload.GithubRepo, Name + ".zip");
+                        break;
+                    case "deviantart":
+                        _downloadHandler = new DeviantArtDownload(FileDownload.DeviantURL, Name + ".zip", RemoteVersionInfo.MatchPattern,
+                            RemoteVersionInfo.MatchGroup, RemoteVersionInfo.MatchURL);
+                        break;
+                    case "direct":
+                        break;
+                    default:
+                        throw new Exception("Unknown download method for skin " + Name);
+                }
+            }
+        }
+
+        public int Install() {
+            int result = Download();
+            if (result != 0) {
+                return result;
+            }
+            result = Unpack();
+            if (result != 0) {
+                return result;
+            }
+            result = CopyToSkinFolder();
+            if(result != 0) {
+                return result;
+            }
+            return 0;
+        }
+
+        public int Download() {
+            try {
+                CreateDownloadHandler();
+            } catch (Exception) {
+                return 1;
+            }
+            _downloadHandler.GetFile();
+            return !File.Exists(Path.Combine("Downloads", Name + ".zip")) ? 2 : 0;
+        }
+
+        public int Unpack() {
+            return 0;
+        }
+
+        public int CopyToSkinFolder() {
+            return 0;
+        }
+
+        public int InstallFonts(List<ExtraInfo.Font> fontList) {
+            foreach (ExtraInfo.Font font in fontList) {
+                
+            }
+            return 0;
+        }
+
+        public string GetRemoteVersion() {
+            try {
+                CreateDownloadHandler();
+            } catch(Exception) {
+                return null;
+            }
+            return _downloadHandler.GetLatestVersionString();
         }
     }
 }
