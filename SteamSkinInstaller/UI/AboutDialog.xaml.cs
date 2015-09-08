33e3b3ad32f8c3fa70927b429f9c9214f61b2d1f
Rico Dittrich <ricod1996@gmail.com>
2015-09-08 09:49:03 +0200

    Woops, forgot to add the AboutDialog

---
+++ SteamSkinInstaller/UI/AboutDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Windows;
+
+namespace SteamSkinInstaller.UI {
+    /// <summary>
+    /// Interaction logic for AboutDialog.xaml
+    /// </summary>
+    public partial class AboutDialog : Window {
+        public AboutDialog() {
+            InitializeComponent();
+
+            TextVersion.Text = TextVersion.Text.Replace("%versionplaceholder%", Assembly.GetExecutingAssembly().GetName().Version.ToString());
+        }
+    }
+}
