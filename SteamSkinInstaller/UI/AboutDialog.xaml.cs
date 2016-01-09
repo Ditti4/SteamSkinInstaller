0a851c1881db7f8371856894862560624cfbd96d
Rico Dittrich <ricod1996@gmail.com>
2016-01-09 22:03:05 +0100

    Remove developers textarea
    It only featured me anyway and I felt selfish because of that.

--- SteamSkinInstaller/UI/AboutDialog.xaml.cs
+++ SteamSkinInstaller/UI/AboutDialog.xaml.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Reflection;
-using System.Windows;
 using System.Windows.Navigation;
 
 namespace SteamSkinInstaller.UI {
@@ -10,9 +8,6 @@ namespace SteamSkinInstaller.UI {
     /// </summary>
     public partial class AboutDialog {
         private string _codename = "Knorke";
-        private readonly string[] _developers = {
-            "Rico \"Ditti4\" Dittrich",
-        };
 
         public AboutDialog() {
             InitializeComponent();
@@ -21,10 +16,6 @@ namespace SteamSkinInstaller.UI {
                              Assembly.GetExecutingAssembly().GetName().Version.Minor + " \"" + _codename + "\"";
 
             TextVersion.Text = TextVersion.Text.Replace("%versionplaceholder%", version);
-
-            foreach (string dev in _developers) {
-                BoxDevelopers.AppendText(dev + Environment.NewLine);
-            }
         }
 
         private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e) {
